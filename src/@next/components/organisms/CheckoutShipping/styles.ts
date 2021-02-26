9f6621804a92f16608796d4e2211e7a6a0ec8451
VinceJuliano <VinceJuliano>
2021-02-26 10:48:01 -0500

    shipping page message

--- src/@next/components/organisms/CheckoutShipping/styles.ts
+++ src/@next/components/organisms/CheckoutShipping/styles.ts
@@ -7,6 +7,11 @@ export const ShippingMethodForm = styled.form`
 
 export const Title = styled.h3`
   font-weight: ${props => props.theme.typography.boldFontWeight};
+  padding: 0 0 0.6rem 0;
+`;
+
+export const ShippingInfo = styled.div`
+  font-weight: ${props => props.theme.typography.boldFontWeight};
   padding: 0 0 1.6rem 0;
 `;
 
